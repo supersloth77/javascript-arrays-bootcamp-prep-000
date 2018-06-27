3c3afc7b30d546c2ef541b29f99263bd8f290fc0
Peter Traversa <peter.traversa@gmail.com>
2018-06-27 16:29:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(addition, chocolateBars) {
+function addElementToBeginningOfArray(chocolateBars, addition) {
   return [addition, ...chocolateBars];
 }
 
