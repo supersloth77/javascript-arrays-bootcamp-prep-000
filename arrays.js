555364f05806a80208857ee4b7736fa4017001d5
Peter Traversa <peter.traversa@gmail.com>
2018-06-27 16:30:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,8 +3,3 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(chocolateBars, addition) {
   return [addition, ...chocolateBars];
 }
-
-function destructivelyAddElementToBeginningOfArray(chocolateBars, addition) {
-  chocolateBars = chocolateBars.unshift(addition);
-  return chocolateBars;
-}
